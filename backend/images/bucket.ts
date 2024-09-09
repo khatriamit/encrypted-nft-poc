532ef1186ceee528a41b7138b17cdd3f433ca15d
khatriamit <khatriamit054@gmail.com>
2024-09-09 12:30:57 +0545

    update gitignore

--- backend/images/bucket.ts
+++ backend/images/bucket.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { S3, PutObjectCommand, ObjectCannedACL } from "@aws-sdk/client-s3";
+import axios from "axios";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -18,6 +19,8 @@ const s3Client = new S3({
     secretAccessKey: process.env.BUCKET_SECRET!,
   },
 });
+const basePublisherUrl = "http://127.0.0.1:31415";
+const baseAggregatorUrl = "http://127.0.0.1:31416";
 
 export async function uploadImage(image: string, name: string): Promise<string> {
   const buf = Buffer.from(image.replace(/^data:image\/\w+;base64,/, ""), 'base64');
@@ -44,6 +47,61 @@ export async function uploadCiphertext(cipher: string, imageName: string): Promi
     };
     const result = await s3Client.send(new PutObjectCommand(params));
     return `${endpoint}/${bucket}/${path}`;
+}
+
+
+// Walrus storage mechanism
+export async function uploadCipherTextWal(cipher: string, imageName: string): Promise<string> {
+  // console.log("UPLOADING to WALRUS!!!");
+  // const blob = new Blob([cipher], {type: 'text/plain'});
+  // const file = new File([blob], `${imageName}.txt`, { type: 'text/plain' });
+  // const formData = new FormData();
+  // formData.append('file', file);
+
+  // fs.writeFile("test.txt", cipher, (err) => {
+  //   if (err) {
+  //       console.error('Error writing file:', err);
+  //   } else {
+  //       console.log('File has been saved successfully.');
+  //   }
+  // });
+
+
+  const url = `${basePublisherUrl}/v1/store?epochs=1`;
+  try{
+    // const res = await axios.put(url, {cipher, imageName});
+    console.log("Length: ", cipher.length);
+    const res = await axios.put(url, {cipher});
+    console.log('File uploaded successfully:', res.data);
+    const blobID = res.data.newlyCreated.blobObject.blobId;
+    console.log(res.status);
+    return `${baseAggregatorUrl}/v1/${blobID}`;
+  }catch (e){
+    console.log("ERROR: ", e);
+    return e
+  }
+}
+
+export async function uploadImageWL(image: string, name: string): Promise<string> {
+  const buf = Buffer.from(image.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+  const response = await fetch(`${basePublisherUrl}/v1/store`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'image/png',
+      'Content-Length': buf.length.toString(),
+      'Content-Disposition': `attachment; filename="${name}"`,
+    },
+    body: buf,
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to upload image: ${response.statusText}`);
+  }
+
+  const responseBody = await response.json();
+  const blobID = responseBody.newlyCreated.blobObject.blobId;
+  console.log(responseBody);
+  return `${baseAggregatorUrl}/v1/${blobID}`;
 }
 
 export async function deleteItem(path: string): Promise<void> {
